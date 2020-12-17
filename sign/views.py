c6a593196dded7632abdeca405bd6db4f434d89f
mei <3396790998@qq.com>
2020-12-17 12:22:06 +0800

    Django模板 - 前端页面开发：列表、分页、搜索、签到功能的实现

--- sign/views.py
+++ sign/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from sign.models import Event, Guest
 
 
 # Create your views here.
@@ -14,29 +16,114 @@ def login_action(request):
     if request.method == 'POST':
         username = request.POST.get('username', '') #对应form表单中的<input>标签的name属性
         password = request.POST.get('password', '')
-        '''
-        if username == 'admin' and password == 'admin123':
-            # return HttpResponse('login success!')
-
-            # return HttpResponseRedirect('/event_manage/')            
-        '''
         user = auth.authenticate(username=username, password=password)
         if user is not None:
             auth.login(request, user) # 登录
 
             request.session['user'] = username  # 将session信息记录到浏览器
-
-            # response.set_cookie('user', username, 3600) # 添加浏览器cookie,保持时间3600秒
             response = HttpResponseRedirect('/event_manage/')
             return response
         else:
             return render(request, 'index.html', {'error':'username or password error!'}) # 返回错误提示的字典
 
+
+# 退出登录
+@login_required
+def logout(request):
+    auth.logout(request)
+    response = HttpResponseRedirect('/index/')
+    return response
+
 # 发布会管理
 @login_required
 def event_manage(request):
-    # return render(request, "event_manage.html")
+    event_list = Event.objects.all()
+    username = request.session.get('user', '')  # 读取浏览器session
+    return render(request, "event_manage.html", {"user": username,
+                                                 "events": event_list})
 
-    # username = request.COOKIES.get('user', '') # 读取浏览器cookie
-    username = request.session.get('user', '') # 读取浏览器session
-    return render(request, "event_manage.html", {"user" : username})
+# 发布会名称搜索
+@login_required
+def search_name(request):
+    username = request.session.get('user', '')
+    search_name = request.GET.get('name', '')
+    event_list = Event.objects.filter(name__contains=search_name)
+    return render(request, "event_manage.html", {"user": username,
+                                                 "events": event_list})
+
+
+# 嘉宾管理
+@login_required
+def guest_manage(request):
+    username = request.session.get('user', '')
+    guest_list = Guest.objects.all()
+    paginator = Paginator(guest_list, 2)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # 如果page不是整数，取第一页面数据
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # 如果page不在范围，取最后一页面
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, "guest_manage.html", {"user": username,
+                                                 "guests": contacts})
+
+
+# 嘉宾姓名搜索
+@login_required
+def search_realname(request):
+    username = request.session.get('user', '')
+    guest_search_name = request.GET.get('realname', '')
+    guest_list = Guest.objects.filter(realname__contains=guest_search_name)
+    paginator = Paginator(guest_list, 2)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # 如果page不是整数，取第一页面数据
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # 如果page不在范围，取最后一页面
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, "guest_manage.html", {"user": username,
+                                                 "guests": contacts})
+
+
+# 签到页面
+@login_required
+def sign_index(request, eid):
+    event = get_object_or_404(Event, id=eid)
+    guest_list = Guest.objects.filter(event_id__exact=eid)
+    return render(request, "sign_index.html", {"event": event,
+                                               "guests": guest_list,
+                                               "guests_count": len(guest_list)})
+
+
+# 签到动作
+@login_required
+def sign_index_action(request, eid):
+    event = get_object_or_404(Event, id=eid)
+    phone = request.POST.get('phone', '')
+    print(phone)
+    result = Guest.objects.filter(phone=phone)
+    if not result:
+        return render(request, "sign_index.html", {"event": event,
+                                                   "hint": "phone error."})
+
+    result = Guest.objects.filter(phone=phone, event_id=eid)
+    if not result:
+        return render(request, "sign_index.html", {"event": event,
+                                                   "hint": "event id or phone error."})
+
+    for guest in result:
+        print(guest.realname)
+        print(guest.sign)
+        if guest.sign:
+            return render(request, "sign_index.html", {"event": event,
+                                                       "hint": "user has sign in."})
+        else:
+            return render(request, "sign_index.html", {"event": event,
+                                                       "hint": "sign in success!",
+                                                       "guest": result})
